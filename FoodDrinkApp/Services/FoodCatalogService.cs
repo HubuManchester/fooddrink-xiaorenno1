990845637f0e1200b4dea1fa5895f7ba752bb04d
546892172 <546892172@qq.com>
2026-05-30 22:27:18 +0800

    add

--- FoodDrinkApp/Services/FoodCatalogService.cs
+++ FoodDrinkApp/Services/FoodCatalogService.cs
@@ -29,7 +29,7 @@ public static class FoodCatalogService
             Fat = 8,
             AllergyNote = "Contains dairy and gluten.",
             Tags = "healthy breakfast yogurt berries",
-            ImageUrl = "https://images.unsplash.com/photo-1488477181946-6428a0291777?w=400&h=300&fit=crop"
+            ImageUrl = "Resources/Images/Berry Yogurt Bowl.jpg"
         },
         new()
         {
